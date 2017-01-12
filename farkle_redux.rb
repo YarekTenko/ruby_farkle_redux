f169a32f79711191a0656fafcc8f23de3f37c9c5
YarekTenko <yarektenko@gmail.com>
2017-01-12 09:22:03 -0800

    Defined PLayer and Dice classes

--- farkle_redux.rb
+++ farkle_redux.rb
@@ -2,10 +2,27 @@
 class Farkle
   # Player class contains all the player information
   class Player
+    attr_reader   :p_id
+    attr_accessor :p_score
+
+    def initialize(p_id)
+      @p_id    = p_id
+      @p_score = 0
+    end
   end
 
   # Dice class emulates dice and die rolling
   class Dice
+    attr_reader :d_num
+
+    def initialize(d_num)
+      @d_num = d_num
+    end
+    
+    def roll
+      Random.new_seed
+      Array.new(@d_num) { rand(6) + 1 }
+    end
   end
 
   # Game class contains the main game logic
