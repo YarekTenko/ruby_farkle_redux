668292de9fb2c96b9fb8912a320b634d0112901e
YarekTenko <yarektenko@gmail.com>
2017-01-12 09:08:39 -0800

    Added main classes inside Farkle class

--- farkle_redux.rb
+++ farkle_redux.rb
@@ -1,3 +1,18 @@
-# This class represents the dice game Farkle
+# Farkle class represents the dice game Farkle
 class Farkle
+  # Player class contains all the player information
+  class Player
+  end
+
+  # Dice class emulates dice and die rolling
+  class Dice
+  end
+
+  # Game class contains the main game logic
+  class Game
+  end
+
+  # ConsoleMessenger class outputs messages to console
+  class ConsoleMessenger
+  end
 end
